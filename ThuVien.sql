cb03eca7a87dd91d418e02b5fb6c4eaf503d6e75
Admin <itno173qb@example.com>
2025-11-01 21:32:13 +0700

    lam lai

---
+++ ThuVien.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE BOOK (
+    id INT PRIMARY KEY,             
+    name NVARCHAR(255) NOT NULL,     
+    publish DATE,                    
+    author NVARCHAR(150),            
+    year INT                         
+);
+GO
